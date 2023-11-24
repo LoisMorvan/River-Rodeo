9155e91a1423ede5b8491ecb945e3230f841e303
MORVAN Loïs <lmorvan6@myges.fr>
2023-11-24 12:33:24 +0100

    Ajout de la connexion à la base de données

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 
+# Load environment variables from the .env file
+load_dotenv()
 
 # instantiate the app
 app = Flask(__name__)
@@ -8,6 +11,15 @@ app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# Load configuration based on environment
+if app.config['ENV'] == 'development':
+    app.config.from_object('config.DevelopmentConfig')
+else:
+    app.config.from_object('config.ProductionConfig')
+
+db = SQLAlchemy(app)
 
 
 # sanity check route
