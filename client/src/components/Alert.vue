bdb2d7c30e491f90c2756a8f9ddad07b2f6e947e
ANDRIATSILAVO Mitchel Steeve <steeveandriatsilavomitchel@gmail.com>
2023-12-06 20:17:15 +0100

    fin tuto

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="alert alert-success" role="alert">{{ message }}</div>
+    <br/>
+  </div>
+</template>
+<script>
+export default {
+  props: ['message'],
+};
+</script>
