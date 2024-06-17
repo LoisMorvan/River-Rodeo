ce87ff79d482a10419af1c56a49f0df1503e5285
ANDRIATSILAVO Mitchel Steeve <steeveandriatsilavomitchel@gmail.com>
2024-06-17 11:34:53 +0200

    ajout pop-pup création rejoindre party suppression bouton "My account" et "Setting"

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,8 +6,28 @@ export default {
         { id: 1, name: 'Friend 1' },
         { id: 2, name: 'Friend 2' },
         { id: 3, name: 'Friend 3' }
-      ]
+      ],
+      showSearchPartyPopup: false,
+      showPlayPopup: false,
+      searchPartyId: '',
+      minAmount: ''
     };
+  },
+  methods: {
+    openSearchPartyPopup() {
+      this.showSearchPartyPopup = true;
+    },
+    openPlayPopup() {
+      this.showPlayPopup = true;
+    },
+    confirmSearchParty() {
+      console.log('Search Party ID:', this.searchPartyId);
+      this.showSearchPartyPopup = false;
+    },
+    confirmPlayParty() {
+      console.log('Min Amount:', this.minAmount);
+      this.showPlayPopup = false;
+    }
   }
 };
 </script>
@@ -16,16 +36,32 @@ export default {
   <div class="home-view" :style="{ backgroundImage: `url(${backgroundImage})` }">
     <div class="header"></div>
     <div class="content">
-      <button class="main-button bordered-button">Play</button>
-      <button class="main-button bordered-button">Search Party</button>
-      <button class="main-button bordered-button">My Account</button>
-      <button class="main-button bordered-button">Settings</button>
+      <button class="main-button bordered-button" @click="openPlayPopup">Play</button>
+      <button class="main-button bordered-button" @click="openSearchPartyPopup">
+        Search Party
+      </button>
     </div>
     <div class="friend-list">
       <h3>Friend List</h3>
       <ul>
         <li v-for="friend in friends" :key="friend.id">{{ friend.name }}</li>
       </ul>
+    </div>
+    <div v-if="showSearchPartyPopup" class="popup">
+      <div class="popup-content">
+        <h3>Search Party</h3>
+        <input type="text" v-model="searchPartyId" placeholder="Enter ID" />
+        <button @click="confirmSearchParty">Confirm</button>
+        <button @click="showSearchPartyPopup = false">Cancel</button>
+      </div>
+    </div>
+    <div v-if="showPlayPopup" class="popup">
+      <div class="popup-content">
+        <h3>Play Party</h3>
+        <input type="number" v-model="minAmount" placeholder="Enter Min Amount" />
+        <button @click="confirmPlayParty">Confirm</button>
+        <button @click="showPlayPopup = false">Cancel</button>
+      </div>
     </div>
   </div>
 </template>
@@ -95,5 +131,30 @@ export default {
   top: 50%;
   transform: translateY(-50%);
   color: #fbf9f9;
+}
+
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  background: #ffffff;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.popup-content input {
+  margin-bottom: 10px;
+  padding: 5px;
+  width: 80%;
 }
 </style>
