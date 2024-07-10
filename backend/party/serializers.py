712e54071405919527683495a146988a4ca1d9f4
MORVAN Loïs <lmorvan6@myges.fr>
2024-07-10 15:59:32 +0200

    Ajout de la gestion des rounds

--- backend/party/serializers.py
+++ backend/party/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
-from .models import Party, Round, Pot
+from .models import Party, Round, Pot, PlayerHand, CommunityCard, Bet, GameState
 
 
 class PartySerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
-        fields = ['id', 'min_amount',
-                  'creator', 'created_at', 'is_active']
+        fields = ['id', 'min_amount', 'creator', 'created_at', 'is_active']
         read_only_fields = ['creator', 'created_at', 'is_active']
 
 
@@ -20,3 +19,29 @@ class PotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pot
         fields = ['id', 'round', 'amount', 'created_at']
+
+
+class PlayerHandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerHand
+        fields = ['id', 'round', 'player', 'cards']
+
+
+class CommunityCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommunityCard
+        fields = ['id', 'round', 'card', 'stage']
+
+
+class BetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bet
+        fields = ['id', 'round', 'player', 'amount', 'created_at']
+
+
+class GameStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameState
+        fields = ['id', 'round', 'current_bet', 'current_turn',
+                  'stage', 'pot', 'active_players', 'player_bets']
+        read_only_fields = ['round', 'current_turn', 'pot', 'player_bets']
