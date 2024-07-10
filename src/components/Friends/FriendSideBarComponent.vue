e5912ab8126897b4a4be2efd468cabf3a77c90e4
Jocelyn portable <jocel1.lhommee@gmail.com>
2024-07-10 11:03:21 +0200

    fix: update ami list when accept invitation

--- src/components/Friends/FriendSideBarComponent.vue
+++ src/components/Friends/FriendSideBarComponent.vue
@@ -1,139 +1,150 @@
 <template>
-    <div class="friend-sidebar">
-      <div class="tabs">
-        <button :class="{ active: activeTab === 'friends' }" @click="activeTab = 'friends'">Amis</button>
-        <button :class="{ active: activeTab === 'invitations' }" @click="activeTab = 'invitations'">Invitations</button>
+  <div class="friend-sidebar">
+    <div class="tabs">
+      <button :class="{ active: activeTab === 'friends' }" @click="activeTab = 'friends'">
+        Amis
+      </button>
+      <button :class="{ active: activeTab === 'invitations' }" @click="activeTab = 'invitations'">
+        Invitations
+      </button>
+    </div>
+    <div class="content">
+      <div v-if="activeTab === 'friends'">
+        <ul>
+          <li v-for="friend in friends" :key="friend.id">{{ friend }}</li>
+        </ul>
       </div>
-      <div class="content">
-        <div v-if="activeTab === 'friends'">
-          <ul>
-            <li v-for="friend in friends" :key="friend.id">{{ friend }}</li>
-          </ul>
-        </div>
-        <div v-if="activeTab === 'invitations'">
-          <ul>
-            <li v-for="invitation in invitations" :key="invitation.id"><span>{{ invitation.from_user }}</span>
-              <button class="ignore-button" @click="ignoreInvitation(invitation.id)">Ignorer</button>
-              <button class="accept-button" @click="acceptInvitation(invitation.id)">Accepter</button>
-            </li>
-          </ul>
-        </div>
+      <div v-if="activeTab === 'invitations'">
+        <ul>
+          <li v-for="invitation in invitations" :key="invitation.id">
+            <span>{{ invitation.from_user }}</span>
+            <button class="ignore-button" @click="ignoreInvitation(invitation.id)">Ignorer</button>
+            <button class="accept-button" @click="acceptInvitation(invitation.id)">Accepter</button>
+          </li>
+        </ul>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import api from '@/axiosInstances';
-  export default {
-    name: 'FriendSideBar',
-    props: {
-    },
-    data() {
-      return {
-        activeTab: 'friends',
-        invitations: [],
-        friends: []
-      };
-    },
-    created() {
+  </div>
+</template>
+
+<script>
+import api from '@/axiosInstances';
+export default {
+  name: 'FriendSideBar',
+  props: {},
+  data() {
+    return {
+      activeTab: 'friends',
+      invitations: [],
+      friends: []
+    };
+  },
+  created() {
     this.fetchInvitations();
     this.fetchFriends();
   },
   methods: {
     fetchFriends() {
-      api.get('/auth/friendship/amis/')
-        .then(response => {
+      api
+        .get('/auth/friendship/amis/')
+        .then((response) => {
           this.friends = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error fetching invitations:', error);
         });
     },
     fetchInvitations() {
-      api.get('/auth/friendship/invitations/')
-        .then(response => {
+      api
+        .get('/auth/friendship/invitations/')
+        .then((response) => {
           this.invitations = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error fetching invitations:', error);
         });
     },
     acceptInvitation(invitationId) {
-      api.post(`/auth/friendship/invitations/${invitationId}/accept/`)
-        .then(response => {
+      api
+        .post(`/auth/friendship/invitations/${invitationId}/accept/`)
+        .then((response) => {
           console.log('Invitation accepted successfully:', response.data);
           // Optionally update UI or fetch invitations again
           this.fetchInvitations();
+          this.fetchFriends();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error accepting invitation:', error);
         });
     },
     ignoreInvitation(invitationId) {
-      api.post(`/auth/friendship/invitations/${invitationId}/reject/`)
-        .then(response => {
+      api
+        .post(`/auth/friendship/invitations/${invitationId}/reject/`)
+        .then((response) => {
           console.log('Invitation rejected successfully:', response.data);
           // Optionally update UI or fetch invitations again
           this.fetchInvitations();
+          this.fetchFriends();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error rejecting invitation:', error);
         });
     }
   }
-  
-  }
-  </script>
-  
-  <style scoped>
-  .friend-sidebar {
-    width: 250px;
-    background-color: #000000;
-    padding: 10px;
-    position: fixed;
-    top: 100px; /* Adjust this value based on your navbar height */
-    right: 0;
-    height: calc(100% - 100px); /* Adjust this value based on your navbar height */
-    overflow-y: auto;
-  }
-  
-  .tabs {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
-  }
-  
-  .tabs button {
-    flex: 1;
-    padding: 10px;
-    border: none;
-    cursor: pointer;
-    background-color: #ddd;
-  }
-  
-  .tabs button.active {
-    background-color: #bbb;
-  }
-  
-  .content {
-    border-top: 1px solid #ccc;
-    padding-top: 10px;
-  }
-  
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-  
-  li {
-    padding: 5px 0;
-  }
+};
+</script>
 
-  .ignore-button,
+<style scoped>
+.friend-sidebar {
+  width: 250px;
+  background-color: #000000;
+  padding: 10px;
+  position: fixed;
+  top: 100px; /* Adjust this value based on your navbar height */
+  right: 0;
+  height: calc(100% - 100px); /* Adjust this value based on your navbar height */
+  overflow-y: auto;
+}
+
+.tabs {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.tabs button {
+  flex: 1;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  background-color: #ddd;
+}
+
+.tabs button.active {
+  background-color: #bbb;
+}
+
+.content {
+  border-top: 1px solid #ccc;
+  padding-top: 10px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  padding: 5px 0;
+}
+
+.ignore-button,
 .accept-button {
   padding: 5px 10px;
   cursor: pointer;
-  transition: background-color 0.3s, border-width 0.3s;
+  transition:
+    background-color 0.3s,
+    border-width 0.3s;
 }
 
 .ignore-button {
@@ -157,5 +168,4 @@
   background-color: #0056b3;
   border-width: 4px;
 }
-  </style>
-  
+</style>
