6d0db71c44c3d29f3ea592abc22f03a7dbb490f6
Jocelyn portable <jocel1.lhommee@gmail.com>
2024-07-10 11:02:35 +0200

    dev: account view initial

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Authentication/LoginView.vue';
-import Register from '../views/Authentication/RegisterView.vue';
-import HomePage from '../views/HomePage.vue';
-import Party from '../views/Party.vue';
+import Login from '@/views/Authentication/LoginView.vue';
+import Register from '@/views/Authentication/RegisterView.vue';
+import HomePage from '@/views/HomePage.vue';
+import Party from '@/views/Party.vue';
+import Account from '@/views/Account/AccountView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,12 @@ const router = createRouter({
       component: Party,
       meta: { showNavbar: false },
       props: true
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: Account,
+      meta: { showNavbar: true }
     }
   ]
 });
