0f25ad27f951f7bfa31e19c9cf1b81bb2169bc3a
Jocelyn portable <jocel1.lhommee@gmail.com>
2024-07-23 12:41:47 +0200

    chore: Update ASGI configuration for WebSocket functionality

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from backend.routing import application as websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Gestion des requêtes HTTP
+    "websocket": AuthMiddlewareStack(
+        websocket_application  # Utilisez directement l'objet URLRouter importé
+    ),
+})
--- backend/routing.py
+++ backend/routing.py
@@ -1,16 +1,9 @@
 from django.urls import path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from party.consumers import PokerConsumer
+from channels.routing import URLRouter
+from .party.consumers import PokerConsumer
 
 websocket_urlpatterns = [
     path('ws/poker/<int:party_id>/', PokerConsumer.as_asgi()),
 ]
 
-application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+application = URLRouter(websocket_urlpatterns)
